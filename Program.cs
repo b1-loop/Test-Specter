57dd40a01e150bf23522ffa3390f4ec4dc00066e
b1-loop <bojidarivanov98@gmail.com>
2025-11-04 16:57:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Test_Specter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            App.Run();
+        }
+    }
+}
